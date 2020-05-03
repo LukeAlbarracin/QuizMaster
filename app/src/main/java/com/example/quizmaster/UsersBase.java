e8d979b21f4247a1f9dd0d06be38e8bc0cff3c39
derek-yn-zhang <derek.yn.zhang@gmail.com>
2020-05-03 15:02:39 -0500

    ui draft

---
+++ app/src/main/java/com/example/quizmaster/UsersBase.java
@@ -0,0 +1,29 @@
+package com.example.quizmaster;
+
+import java.util.ArrayList;
+
+//public class UsersBase {
+
+//    private List<User> mUsers;
+//    private UsersBase() {
+//        mUsers = new ArrayList<>();
+//        for (int i = 0; i < 100; i++) {
+//            User user = new User();
+//            user.setFirstName("User #" + i);
+//            user.setEmail("user_" + i + "@gmail.com");
+//            mUsers.add(user);
+//        }
+//    }
+//
+//    public List<User> getUsers() {
+//        return mUsers;
+//    }
+//
+//    private static UsersBase mUserBase;
+//    public static UsersBase get() {
+//        if (mUserBase == null) {
+//            mUserBase = new UsersBase();
+//        }
+//        return mUserBase;
+//    }
+//}
---
+++ app/src/main/java/com/example/quizmaster/UsersDatabaseHelper.java
@@ -0,0 +1,29 @@
+//package com.example.quizmaster;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+//public class UsersDatabaseHelper extends SQLiteOpenHelper {
+//
+//    // Constructor will create new database file if it did not already exist
+//    public UsersDatabaseHelper(@Nullable Context context) {
+//        super(context, "usersDatabase.db", null, 1);
+//    }
+//
+//    @Override
+//    public void onCreate(SQLiteDatabase db) {
+//        db.execSQL("create table users(" +
+//                "_id integer primary key autoincrement, " +
+//                "_uuid," +
+//                "first_name," +
+//                "email_address)");
+//    }
+//
+//    @Override
+//    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//
+//    }
+//}
